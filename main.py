ab14db798518cf974188a6b81124716544585c47
Lynne Munini <muninilynne65@gmail.com>
2021-10-09 22:58:58 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from turtle import Turtle, Screen
+import random
+tim = Turtle()
+screen = Screen()
+screen.colormode(255)
+screen.bgcolor("seashell")
+tim.shape("circle")
+tim.shapesize(0.1, 0.1)
+tim.speed("fastest")
+
+
+def circle():
+    radius = 100
+    tim.circle(radius)
+
+
+for _ in range(80):
+    circle()
+    x = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    tim.pencolor(x)
+    tim.color(x)
+    tim.left(5)
+
+
+screen.exitonclick()
